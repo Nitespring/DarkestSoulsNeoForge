28c6051f4a2919b5b88eb6ad94de98a5b97f0a5c
Nitespring <96525113+Nitespring@users.noreply.github.com>
2024-09-02 11:22:19 +0200

    asset dump

--- src/main/java/github/nitespring/darkestsouls/core/init/ItemInit.java
+++ src/main/java/github/nitespring/darkestsouls/core/init/ItemInit.java
@@ -108,7 +108,7 @@ public class ItemInit {
 	public static final DeferredHolder<Item,Scimitar> HUNTSMAN_CUTLASS = ITEMS.register("huntsman_cutlass",
 			() -> new Scimitar(Tiers.TITANITE, 5.0f, 1.9f,3.0f, -0.2f, 4, 0,0,0,0,0,0,0,0,225,8,0.11f, 2,new Item.Properties()));
 	public static final DeferredHolder<Item,Weapon> HUNTSMAN_PITCHFORK = ITEMS.register("huntsman_pitchfork",
-			() -> new Spear(Tiers.TITANITE, 5.0f, 1.9f,3.8f, -0.2f, 4, 225,0,0,0,0,0,0,0,0,8,0.12f, 2,new Item.Properties()));
+			() -> new Spear(Tiers.TITANITE, 5.0f, 1.9f,3.8f, -0.2f, 4, 0,0,0,0,0,0,0,0,225,8,0.12f, 2,new Item.Properties()));
 	public static final DeferredHolder<Item,Weapon> CHURCH_SCYTHE= ITEMS.register("church_scythe",
 			() -> new ChurchScythe(Tiers.TITANITE, 7.5f, 1.6f,4.5f, 0.2f, 7, 0,0,0,0,0,0,1,0,325,12,0.105f, 5,new Item.Properties()));
 	public static final DeferredHolder<Item,Weapon> CHURCH_SCYTHE_UNLIT = ITEMS.register("church_scythe_unlit",
