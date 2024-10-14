2bcf640adc0bcc06dfdb51c6863471d7cf5d6b57
Nitespring <96525113+Nitespring@users.noreply.github.com>
2024-10-14 20:50:10 +0200

    Adding Huntsman Dwelling Structure

--- src/main/java/github/nitespring/darkestsouls/core/init/BlockInit.java
+++ src/main/java/github/nitespring/darkestsouls/core/init/BlockInit.java
@@ -23,6 +23,7 @@ import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.levelgen.structure.TerrainAdjustment;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.material.PushReaction;
 import net.neoforged.neoforge.registries.DeferredHolder;
